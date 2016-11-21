dc801e2bf9418cd038c4c012d4f17c2054098bed
Yang SHEN <bizbetter@outlook.com>
2016-11-21 14:23:57 +0800

    Remove Action of Court. It is for people.

--- MTAServiceStatus/Court.cs
+++ MTAServiceStatus/Court.cs
@@ -9,15 +9,6 @@ namespace MTAServiceStatus
         public bool IsPublished { get; set; }
 
         public bool IsReserved { get; set; }
-
-        public void Publish()
-        {
-            IsPublished = true;
-        }
-
-        public void Reserve()
-        {
-            IsReserved = true;
-        }
+       
     }
 }
