9e41ebba311f3c7ac4c946eb0f8c0b41a05fc58c
jackyzfl <zflzhangfengli@sina.com>
2016-11-21 13:58:42 +0800

    first resevation case

---
+++ src/Player.cpp
@@ -0,0 +1,9 @@
+//
+// Created by zhangfl on 2016/11/21.
+//
+
+#include "Player.h"
+
+Reservation *Player::Reserve() {
+    return nullptr;
+}
---
+++ src/Player.h
@@ -0,0 +1,14 @@
+//
+// Created by zhangfl on 2016/11/21.
+//
+
+#ifndef CSD_TRAINING_PRJ_CPLUSPLUS_2016NOV_PLAYER_H
+#define CSD_TRAINING_PRJ_CPLUSPLUS_2016NOV_PLAYER_H
+#include "Reservation.h"
+
+class Player
+{
+public:
+    Reservation* Reserve();
+};
+#endif //CSD_TRAINING_PRJ_CPLUSPLUS_2016NOV_PLAYER_H
