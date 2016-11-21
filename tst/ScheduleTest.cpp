47240e831ddb5fd82a6d7110028aa2911029d429
YunrayZhang <yunrayzhang928@aliyun.com>
2016-11-21 15:50:35 +0800

    add new test for Schedule.cpp

---
+++ tst/ScheduleTest.cpp
@@ -0,0 +1,16 @@
+//
+// Created by caryzhang on 16-11-21.
+//
+
+#include <CppUTest/UtestMacros.h>
+#include <CppUTest/Utest.h>
+#include "Schedule.h"
+TEST_GROUP(ScheduleTest)
+{
+
+};
+
+TEST(ScheduleTest, Given_A_Full_Schedule_When_Reserve_Then_Schedule_Changed)
+{
+    Schedule tmpSchedule;
+}
