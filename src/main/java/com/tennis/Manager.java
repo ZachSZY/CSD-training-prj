0615769f111a3f45e9021fd7cee38f0ad32cea3b
Yang Shuan <shuayang@paypal.com>
2016-11-21 16:25:05 +0800

    publish new court

---
+++ src/main/java/com/tennis/Manager.java
@@ -0,0 +1,10 @@
+package com.tennis;
+
+/**
+ * Created by shuayang on 16/11/21.
+ */
+public class Manager {
+    public boolean publish(Court court) {
+        return true;
+    }
+}
---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,18 @@
+import com.tennis.Court;
+import com.tennis.Manager;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by shuayang on 16/11/21.
+ */
+public class ManagerTest {
+    @Test
+    public void GIVEN_a_court_manager_WHEN_publish_a_court_THEN_succeed() throws Exception {
+        Manager manager = new Manager();
+
+        boolean result = manager.publish(new Court());
+        assertTrue(result);
+    }
+}
