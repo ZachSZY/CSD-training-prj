862442dfb7c98117d27555a02e0160050b8cf279
jackyzfl <zflzhangfengli@sina.com>
2016-11-21 15:41:10 +0800

    find nearest court

--- tst/TennisReservation.cpp
+++ tst/TennisReservation.cpp
@@ -4,6 +4,7 @@
 #include "CppUTest/TestHarness.h"
 #include "Player.h"
 #include "Duartion.h"
+#include "Court.h"
 
 static Player* player;
 TEST_GROUP(tennis)
@@ -24,8 +25,19 @@ TEST(tennis, GEVIN_tennis_player_WHEN_reserve_THEN_get_one_reservation)
     std::string start_time = "2016-11-21 14:00;00";
     std::string end_time =  "2016-11-21 16:00;00";
     Duartion duartion(start_time, end_time);
-    Reservation * newReservation = player->Reserve(duartion);
+    Court court;
+    Reservation * newReservation = player->Reserve(duartion, court);
     CHECK_TRUE(NULL != newReservation);
+}
+
+TEST(tennis, GEVIN_tennis_player_WHEN_find_nearest_court_THEN_get_the_nearest_court)
+{
+    //given
+    //when
+    Court court = player->find_nearest_court();
+    //then
+    CHECK_TRUE(court.isValid());
+
 }
 
 
