01f9065aa890cee94919c5e21596fab54735b7e8
jackyzfl <zflzhangfengli@sina.com>
2016-11-21 14:38:25 +0800

    fix build fail NULL

--- tst/TennisReservation.cpp
+++ tst/TennisReservation.cpp
@@ -19,8 +19,9 @@ TEST_GROUP(tennis)
 
 TEST(tennis, GEVIN_tennis_player_WHEN_reserve_THEN_get_one_reservation)
 {
-    std::string reservation_time = "2016-11-21 14:00;00p";
-    Reservation * newReservation = player->Reserve(reservation_time);
+    std::string start_time = "2016-11-21 14:00;00";
+    std::string end_time =  "2016-11-21 16:00;00";
+    Reservation * newReservation = player->Reserve(start_time, end_time);
     CHECK_TRUE(NULL != newReservation);
 }
 
