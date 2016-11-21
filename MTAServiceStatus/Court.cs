0c1f3068969bf40770571dd983b5e3052184d743
Celery Liu <liuct94@gmail.com>
2016-11-21 16:17:07 +0800

    remove unused code

--- MTAServiceStatus/Court.cs
+++ MTAServiceStatus/Court.cs
@@ -1,16 +1,10 @@
-﻿using System;
-
-namespace MTAServiceStatus
+﻿namespace MTAServiceStatus
 {
     public class Court
     {
         public Court() { }
 
         public int CourtID { get; set; }
-
-        public DateTime OccupyTimeStart { get; set; }
-
-        public int OccupiedHours { get; set; }
 
         public decimal FeePerHour { get; set; }
 
