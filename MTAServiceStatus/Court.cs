3347c20bd890b4f677ec7c4ce9a78cb589f964a1
Yang SHEN <bizbetter@outlook.com>
2016-11-21 14:55:24 +0800

    Reserve

--- MTAServiceStatus/Court.cs
+++ MTAServiceStatus/Court.cs
@@ -6,6 +6,14 @@ namespace MTAServiceStatus
     {
         public Court() { }
 
+        public int CourtID { get; set; }
+
+        public DateTime OccupyTimeStart { get; set; }
+
+        public int OccupiedHours { get; set; }
+
+        public decimal FeePerHour { get; set; }
+
         public bool IsPublished { get; set; }
 
         public bool IsReserved { get; set; }
